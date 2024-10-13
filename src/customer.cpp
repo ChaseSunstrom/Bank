880cbf39657942b68c38c3ac5495a02967a19217
DethByBaguette <145382322+DethByBaguette@users.noreply.github.com>
2024-10-13 17:54:23 -0500

    Create customer.cpp
    provides a use for customer class

---
+++ src/customer.cpp
@@ -0,0 +1,35 @@
+#ifndef CUSTOMER_HPP
+#define CUSTOMER_HPP
+
+#include <string>
+#include <iostream>
+#include "types.hpp"  
+
+namespace Bank {
+
+class Customer {
+public:
+    Customer(i32 id, const std::string& name)
+        : m_customer_id(id), m_customer_name(name) {}
+    
+    i32 GetCustomerId() const {
+        return m_customer_id;
+    }
+    
+    std::string GetCustomerName() const {
+        return m_customer_name;
+    }
+    
+    void DisplayCustomerInfo() const {
+        std::cout << "Customer ID: " << m_customer_id << std::endl;
+        std::cout << "Customer Name: " << m_customer_name << std::endl;
+    }
+    
+private:
+    i32 m_customer_id;
+    std::string m_customer_name;
+};
+
+} 
+
+#endif 
